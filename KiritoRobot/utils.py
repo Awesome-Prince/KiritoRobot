ed45ff1354c1bddacff9069469f45a5b30cc0566
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-07-22 18:00:58 +0530

    bumped tele version

--- KiritoRobot/utils.py
+++ KiritoRobot/utils.py
@@ -1,17 +1,97 @@
 # Work Done By < @Awesome-Prince >
 # // @BlackLoverNetwork //
 
-import sys
+import glob
 import logging
-import importlib
+import sys
 from pathlib import Path
 
-def load_plugins(plugin_name):
-    path = Path(f"KiritoRobot/plugins/{plugin_name}.py")
-    name = "KiritoRobot.plugins.{}".format(plugin_name)
-    spec = importlib.util.spec_from_file_location(name, path)
-    load = importlib.util.module_from_spec(spec)
-    load.logger = logging.getLogger(plugin_name)
-    spec.loader.exec_module(load)
-    sys.modules["KiritoRobot.plugins." + plugin_name] = load
-    print("[KIRITOROBOT] HAS IMPORTED " + plugin_name)
+from telethon import events
+
+from . import OWNER_ID, BlackLover
+
+
+def KiritoRobot(**args):
+    pattern = args.get("pattern", None)
+    r_pattern = r"^[/?!+]"
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+    args["pattern"] = pattern.replace("^/", r_pattern, 1)
+
+    def decorator(func):
+        async def wrapper(check):
+            if check.sender_id and check.sender_id != OWNER_ID:
+                pass
+            try:
+                await func(check)
+            except BaseException:
+                return
+            else:
+                pass
+
+        tbot.add_event_handler(wrapper, events.NewMessage(**args))
+        return wrapper
+
+    return decorator
+
+
+def query(**args):
+    pattern = args.get("pattern", None)
+
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    def decorator(func):
+        tbot.add_event_handler(func, events.InlineQuery(**args))
+        return func
+
+    return decorator
+
+
+def Cinline(**args):
+    def decorator(func):
+        tbot.add_event_handler(func, events.CallbackQuery(**args))
+        return func
+
+    return decorator
+
+
+def load_module(shortname):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        import importlib
+
+        import KiritoRobot.utils  # pylint:disable=E0602
+
+        path = Path(f"KiritoRobot/plugins/{shortname}.py")
+        name = "KiritoRobot.plugins.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        print("[KIRITOROBOT] Successfully Imported " + shortname)
+    else:
+        import importlib
+
+        import KiritoRobot.utils  # pylint:disable=E0602
+
+        path = Path(f"KiritoRobot/plugins/{shortname}.py")
+        name = "KiritoRobot.plugins.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.KiritoRobot = KiritoRobot
+        mod.tbot = tbot
+        mod.logger = logging.getLogger(shortname)
+        mod.THUMB = THUMB
+        spec.loader.exec_module(mod)
+        sys.modules["KiritoRobot.plugins." + shortname] = mod
+        print("[KIRITOROBOT] Successfully Imported " + shortname)
+
+
+path = "KiritoRobot/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        load_module(shortname.replace(".py", ""))
