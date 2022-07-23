f95aa2d9e019149ed62652f4f0508686dc96bea5
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-07-23 15:36:49 +0530

    Update wish.py

--- KiritoRobot/plugins/wish.py
+++ KiritoRobot/plugins/wish.py
@@ -8,7 +8,7 @@ from KiritoRobot import tbot
 @tbot.on(events.NewMessage(pattern="/wish ?(.*)"))
 async def wish(e):
 
-    if e.is_reply:
+    if not e.is_reply:
         mm = random.randint(1, 100)
         lol = await e.get_reply_message()
         fire = "https://telegra.ph/file/049090e104b219c9db580.mp4"
@@ -18,7 +18,7 @@ async def wish(e):
             caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your wish has been cast.🖤**\n\n__chance of success {mm}%__",
             reply_to=lol,
         )
-    if not e.is_reply:
+    if e.is_reply:
         fire = "https://telegra.ph/file/049090e104b219c9db580.mp4"
         await tbot.send_file(
             e.chat_id,
