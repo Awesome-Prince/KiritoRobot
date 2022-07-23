d3edfc6a9a0ee47290d190ded14665925410c5b3
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-07-23 10:25:48 +0530

    Just a try to add pic

--- KiritoRobot/plugins/start.py
+++ KiritoRobot/plugins/start.py
@@ -8,7 +8,7 @@ from KiritoRobot.utils import KiritoRobot
 @KiritoRobot(pattern=("/start"))
 async def start(event):
     await event.reply(
-        "Suprise Suprise Mother Fucker, The King Is Back!",
+        "Suprise Suprise Mother Fucker, The King Is Back[!](https://telegra.ph/file/36dd96df77bc2b7ef9b3d.png)",
         buttons=[
             [Button.url("Support", url="https://t.me/blacklover_support")],
             [Button.inline("Help", data="help")],
