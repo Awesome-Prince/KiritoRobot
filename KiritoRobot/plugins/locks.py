4c8285686f9e1658fd063d61e71e1f8d73eb5e44
「 LovelyPrince™ 」 <harishk2appu@gmail.com>
2022-07-24 14:41:03 +0530

    fixed typo

--- KiritoRobot/plugins/locks.py
+++ KiritoRobot/plugins/locks.py
@@ -10,7 +10,7 @@ LOCKS_HELP = """
 ‣ `/locktypes` - To get a list of modules can be locked
 """
 
-@Stark.on(events.NewMessage(pattern="^[!?/]lock ?(.*)"))
+@tbot.on(events.NewMessage(pattern="^[!?/]lock ?(.*)"))
 @is_admin
 async def lock(event, perm):
     if not perm.change_info:
@@ -21,34 +21,34 @@ async def lock(event, perm):
        await event.reply("You haven't specified anything to lock.")
        return
     if "text" in input_str:
-       await Stark.edit_permissions(event.chat_id, send_messages=False)
+       await tbot.edit_permissions(event.chat_id, send_messages=False)
        await event.reply("Locked `text`.")
     elif "media" in input_str:
-       await Stark.edit_permissions(event.chat_id, send_media=False)
+       await tbot.edit_permissions(event.chat_id, send_media=False)
        await event.reply("Locked `media`.")
     elif "sticker" in input_str:
-       await Stark.edit_permissions(event.chat_id, send_stickers=False)
+       await tbot.edit_permissions(event.chat_id, send_stickers=False)
        await event.reply("Locked `sticker`.")
     elif "gifs" in input_str:
-       await Stark.edit_permissions(event.chat_id, send_gifs=False)
+       await tbot.edit_permissions(event.chat_id, send_gifs=False)
        await event.reply("Locked `gifs`.")
     elif "forward" in input_str:
-       await Stark.edit_permissions(event.chat_id, forwards=False)
+       await tbot.edit_permissions(event.chat_id, forwards=False)
        await event.reply("Locked `forward`.")
     elif "games" in input_str:
-       await Stark.edit_permissions(event.chat_id, send_games=False)
+       await tbot.edit_permissions(event.chat_id, send_games=False)
        await event.reply("Locked `games`.")
     elif "inline" in input_str:
-       await Stark.edit_permissions(event.chat_id, send_inline=False)
+       await tbot.edit_permissions(event.chat_id, send_inline=False)
        await event.reply("Locked `inline`.")
     elif "polls" in input_str:
-       await Stark.edit_permissions(event.chat_id, send_polls=False)
+       await tbot.edit_permissions(event.chat_id, send_polls=False)
        await event.reply("Locked `polls`.")
     elif "preview" in input_str:
-       await Stark.edit_permissions(event.chat_id, embed_link_previews=False)
+       await tbot.edit_permissions(event.chat_id, embed_link_previews=False)
        await event.reply("Locked `preview`.")
     elif "all" in input_str:
-       await Stark.edit_permissions(event.chat_id,
+       await tbot.edit_permissions(event.chat_id,
           send_messages=False, 
           send_media=False,
           send_stickers=False,
@@ -60,7 +60,7 @@ async def lock(event, perm):
        await event.reply("Locked `all`.")
 
 
-@Stark.on(events.NewMessage(pattern="^[!?/]unlock ?(.*)"))
+@tbot.on(events.NewMessage(pattern="^[!?/]unlock ?(.*)"))
 @is_admin
 async def unlock(event, perm):
     if not perm.change_info:
@@ -71,25 +71,25 @@ async def unlock(event, perm):
        await event.reply("You haven't specified anything to unlock.")
        return
     if "text" in input_str:
-       await Stark.edit_permissions(event.chat_id, send_messages=True)
+       await tbot.edit_permissions(event.chat_id, send_messages=True)
        await event.reply("Unlocked `text`.")
     elif "media" in input_str:
-       await Stark.edit_permissions(event.chat_id, send_media=True)
+       await tbot.edit_permissions(event.chat_id, send_media=True)
        await event.reply("Unlocked `media`.")
     elif "sticker" in input_str:
-       await Stark.edit_permissions(event.chat_id, send_stickers=True)
+       await tbot.edit_permissions(event.chat_id, send_stickers=True)
        await event.reply("Unlocked `sticker`.")
     elif "gifs" in input_str:
-       await Stark.edit_permissions(event.chat_id, send_gifs=True)
+       await tbot.edit_permissions(event.chat_id, send_gifs=True)
        await event.reply("Unlocked `gifs`.")
     elif "forward" in input_str:
-       await Stark.edit_permissions(event.chat_id, forwards=True)
+       await tbot.edit_permissions(event.chat_id, forwards=True)
        await event.reply("Unlocked `forward`.")
     elif "games" in input_str:
-       await Stark.edit_permissions(event.chat_id, send_games=True)
+       await tbot.edit_permissions(event.chat_id, send_games=True)
        await event.reply("Unlocked `games`.")
     elif "inline" in input_str:
-       await Stark.edit_permissions(event.chat_id, send_inline=True)
+       await tbot.edit_permissions(event.chat_id, send_inline=True)
        await event.reply("Unlocked `inline`.")
     elif "polls" in input_str:
        await tbot.edit_permissions(event.chat_id, send_polls=True)
@@ -98,7 +98,7 @@ async def unlock(event, perm):
        await tbot.edit_permissions(event.chat_id, embed_link_previews=True)
        await event.reply("Unlocked `preview`.")
     elif "all" in input_str:
-       await Stark.edit_permissions(event.chat_id,
+       await tbot.edit_permissions(event.chat_id,
           send_messages=True, 
           send_media=True,
           send_stickers=True,
