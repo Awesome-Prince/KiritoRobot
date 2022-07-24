c63c5a2397decafce19ae5c59c4ca6efd9cb8720
STBxD <87096507+STBxD@users.noreply.github.com>
2022-07-24 12:20:34 +0530

    Update __init__.py

--- KiritoRobot/__init__.py
+++ KiritoRobot/__init__.py
@@ -15,6 +15,9 @@ logging.basicConfig(
     level=logging.INFO,
     handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
 )
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
+)
 LOGGER = logging.getLogger(__name__)
 
 StartTime = time.time()
