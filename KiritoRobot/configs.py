543a544b93fe5a50da839f6f27d68bd473638b9e
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-07-24 20:57:43 +0530

    Update configs.py

--- KiritoRobot/configs.py
+++ KiritoRobot/configs.py
@@ -5,5 +5,5 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", None)
     TOKEN = os.environ.get("TOKEN", None)
     BOT_US = os.environ.get("BOT_US", None)
-    WELCOME_TEXT = os.environ.get("WELCOME_TEXT", None)
+    WELCOME_TEXT = os.environ.get("WELCOME_TEXT", 'Cardinal System Is Damaged!, Sorry I Cant Remember You.')
     RULES = os.environ.get("RULES", None)
