79597b1a92b3da458a8205c1865772b990369353
STBxD <87096507+STBxD@users.noreply.github.com>
2022-07-24 12:19:57 +0530

    Update __main__.py

--- KiritoRobot/__main__.py
+++ KiritoRobot/__main__.py
@@ -1,28 +1,6 @@
-# Work Done By < @Awesome-Prince >
-# // @BlackLoverNetwork //
-
-import glob
-import logging
-from pathlib import Path
-
-from KiritoRobot.utils import load_plugins
-
+import KiritoRobot.utils 
 from . import TOKEN, tbot
 
-logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
-)
-
-path = "KiritoRobot/plugins/*.py"
-files = glob.glob(path)
-for name in files:
-    with open(name) as a:
-        patt = Path(a.name)
-        plugin_name = patt.stem
-        load_plugins(plugin_name.replace(".py", ""))
-
-print("Your Host Is Successfully Done!")
-print("Visit @BlackLover_Support if any error")
 
 try:
     tbot.start(bot_token=TOKEN)
@@ -30,3 +8,5 @@ except Exception as z:
     print(z)
 
 tbot.run_until_disconnected()
+print("Your Host Is Successfully Done!")
+print("Visit @BlackLover_Support if any error")
