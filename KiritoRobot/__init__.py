a2c1720749b64127f33cf8089c29e25350a56150
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-07 19:22:29 +0530

    Update __init__.py

--- KiritoRobot/__init__.py
+++ KiritoRobot/__init__.py
@@ -31,4 +31,4 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 BOT_ID = 5534493283
 
-tbot = TelegramClient(None, API_KEY, API_HASH)
+tbot = TelegramClient('tbot', API_ID, API_HASH).start(token=TOKEN)
