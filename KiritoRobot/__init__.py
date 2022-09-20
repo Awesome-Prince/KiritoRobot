efacab545c7cfa70ee9b3d80550b323d8b461b2f
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-09-20 21:40:42 +0530

    Update __init__.py

--- KiritoRobot/__init__.py
+++ KiritoRobot/__init__.py
@@ -1,5 +1,30 @@
-# By < @Awesome_Prince >
-# // @BlackLoverNeterk //
+"""
+BSD 2-Clause License
+
+Copyright (c) 2022, Awesome-Prince (https://github.com/Awesome-Prince)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
 
 import logging
 import os
@@ -25,8 +50,8 @@ CMD_HELP = {}
 
 TOKEN = os.environ.get("TOKEN")
 OWNER_ID = int(os.environ.get("OWNER_ID", 5362971543))
-API_KEY = int(os.environ.get("API_KEY", 8378464))
-API_HASH = os.environ.get("API_HASH", "0887aac30af224501dd2a470913e990b")
+API_KEY = int(os.environ.get("API_KEY", 12345))
+API_HASH = os.environ.get("API_HASH", "12345")
 DB_URI = os.environ.get("DATABASE_URL", "")
 SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 BOT_ID = 5534493283
