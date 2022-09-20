9a2fe1e82e9ee973c15c1900ceaf044b19729518
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-09-20 21:49:37 +0530

    Update help.py

--- KiritoRobot/plugins/help.py
+++ KiritoRobot/plugins/help.py
@@ -1,3 +1,31 @@
+"""
+BSD 2-Clause License
+
+Copyright (c) 2022, Awesome-Prince (https://github.com/Awesome-Prince)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
 from telethon import Button, events
 
 from KiritoRobot import tbot
@@ -19,8 +47,8 @@ System Commands Available:
 ❍ /start: Cʜᴇᴄᴋ Mᴇ... Iꜰ I Aᴍ Aʟɪᴠᴇ Oʀ Nᴏᴛ
 ❍ /help: Usᴇ Tʜɪs... Iꜰ Yᴏᴜ Nᴇᴇᴅ Mᴏʀᴇ Iɴꜰᴏ Aʙᴏᴜᴛ Mᴇ!
 ━━━━━━━━━━━━━━━━━━━━━━━━
-For Issues Report At @BlackLover_Support
-Powered by @BlackLover_Network [🖥](https://telegra.ph/file/b7d0d2c10a56e3b739611.png)
+For Issues Report At @Programmer_Support
+Powered by @Besties_XD [🖥](https://telegra.ph/file/b7d0d2c10a56e3b739611.png)
 ━━━━━━━━━━━━━━━━━━━━━━━━
 All commands can either be used with /.
 
