c8c79d75de778e2a073b395fd643da9c5e9f2ee8
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-09-21 10:18:01 +0530

    Update nightmode.py

--- KiritoRobot/plugins/nightmode.py
+++ KiritoRobot/plugins/nightmode.py
@@ -1,3 +1,26 @@
+"""
+BSD 2-Clause License
+Copyright (c) 2022, Awesome-Prince (https://github.com/Awesome-Prince)
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from telethon import *
 from telethon import functions
@@ -6,7 +29,7 @@ from telethon.tl.types import ChatBannedRights
 from KiritoRobot import OWNER_ID
 from KiritoRobot import tbot
 from KiritoRobot.events import register
-from KiritoRobot.plugins.helpers.nightmode_helper import (
+from KiritoRobot.plugins.sql.nightmode_sql import (
     add_nightmode,
     get_all_chat_id,
     is_nightmode_indb,
